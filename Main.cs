16e84931bb58aa6bd867cc0381848980bb611c78
Romain De Oliveira <romainpaulo.deoliveira@gmail.com>
2025-08-15 15:28:49 +0200

    fix: clean script

--- Main.cs
+++ Main.cs
@@ -8,7 +8,6 @@ public partial class Main : Node2D
 
     public override void _Ready()
     {
-        GD.Print("Hello World");
         _sprite = GetNode<Sprite2D>("Icon");
         _sprite.Position = _startPosition;
     }
@@ -16,7 +15,6 @@ public partial class Main : Node2D
     public override void _Process(double delta)
     {
         _sprite.Position += new Vector2(450.0f * (float)delta, 0.0f);
-        // Récupère la largeur de la fenêtre où le jeu est affiché
         if (_sprite.Position.X > GetViewport().GetVisibleRect().Size.X + _sprite.Texture.GetWidth())
         {
             _sprite.Position = _startPosition;
